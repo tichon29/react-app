e5922089de5e9ce7328d65c574e0b5d816a8e217
Stephane Hervochon <stephane.hervochon@genesys.com>
2020-10-11 20:41:48 +0200

    fix: fix issue

--- src/release.config.js
+++ src/release.config.js
@@ -4,11 +4,14 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    ["@semantic-release/git", {
-      assets: [
-        { path: "build.zip", label: "Build"},
-        { path: "coverage.zip", label: "Coverage"},
-      ]
-    }]
+    [
+      "@semantic-release/git",
+      {
+        assets: [
+          { path: "build.zip", label: "Build" },
+          { path: "coverage.zip", label: "Coverage" },
+        ],
+      },
+    ],
   ],
 };
