951f1136a6540521ac427066b19d843a1670aef9
Stephane Hervochon <stephane.hervochon@genesys.com>
2020-10-11 18:38:29 +0200

    fix: fix description

---
+++ src/release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: "master",
+  repositoryUrl: "https://github.com/tichon29/react-app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/git"
+  ]
+}
