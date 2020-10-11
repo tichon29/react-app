9d464293cbe8cc182a178a890fc46e0662869d28
Stephane Hervochon <stephane.hervochon@genesys.com>
2020-10-11 18:58:55 +0200

    feat: another feature

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React with Tiche Fix
+          Learn React with Tiche Fix - Feature
         </a>
       </header>
     </div>
