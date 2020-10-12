a0836dbcd6f6dd69a1562972a4cd447f634972b2
Stephane Hervochon <stephane.hervochon@genesys.com>
2020-10-12 07:27:13 +0200

    update app.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React with Tiche
+          Learn e
         </a>
       </header>
     </div>
