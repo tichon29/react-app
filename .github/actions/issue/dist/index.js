3bda6020b2586fd2a454f51e0e35076c6a74ec90
Stephane Hervochon <stephane.hervochon@genesys.com>
2020-10-12 09:17:00 +0200

    fix: tt

--- .github/actions/issue/dist/index.js
+++ .github/actions/issue/dist/index.js
@@ -3576,26 +3576,30 @@ exports.getState = getState;
 const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
-try {
-  const token = core.getInput('token');
-  const title = core.getInput('title');
-  const body = core.getInput('body');
-  const assignees = core.getInput('assignees');
+async function run() {
+  try {
+    const token = core.getInput('token');
+    const title = core.getInput('title');
+    const body = core.getInput('body');
+    const assignees = core.getInput('assignees');
 
-  const octokit = new github.getOctokit(token)
-  const response = octokit.issues.create({
-    // owner: github.context.repo.owner,
-    // repo: github.context.repo.repo
-    ...github.context.repo,
-    title,
-    body,
-    assignees: assignees ? assignees.split('\n') : undefined
-  });
+    const octokit = new github.getOctokit(token)
+    const response = await octokit.issues.create({
+      // owner: github.context.repo.owner,
+      // repo: github.context.repo.repo
+      ...github.context.repo,
+      title,
+      body,
+      assignees: assignees ? assignees.split('\n') : undefined
+    });
 
-  core.setOutput('issue', JSON.stringify(response.data));
-} catch(error) {
-  core.setFailed(error.message)  
+    core.setOutput('issue', JSON.stringify(response.data));
+  } catch(error) {
+    core.setFailed(error.message)  
+  }
 }
+
+run();
 
 /***/ }),
 
