bd49bf434b037db2750c6d41d005304abd5543cb
Stephane Hervochon <stephane.hervochon@genesys.com>
2020-10-12 07:25:07 +0200

    fix: add_issue
    Add issue in case of failure

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React with Tiche Fix - Feature
+          Learn 
         </a>
       </header>
     </div>
